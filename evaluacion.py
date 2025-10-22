deff0dc479a8ff9463334d7c772a4b53edca7c93
Marcos Garcia <marcos92vs@gmail.com>
2025-10-22 09:18:17 +0200

    Añadido el script base 'evaluacion.py'

---
+++ evaluacion.py
@@ -0,0 +1,23 @@
+#AYYOUB
+def pedir_datos():
+    pass
+
+#JUAN
+def calcular_promedio(lista_notas):
+    pass
+
+#DAVID
+def evaluar_aprobado(promedio):
+    pass
+
+#MARCOS
+def calculo_tasas(lista_reporte):
+    pass
+
+#DYAN
+def mostrar_clasificacion(lista_alumnos):
+    pass
+
+#DIEGO
+def mostrar_reporte(lista_reporte):
+    pass
